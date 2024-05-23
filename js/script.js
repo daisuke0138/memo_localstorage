9de45b8d2f96c755ba6f5966a3eccb861fb61b11
04_W_daisuke_aramaki <daisuke_arsp@hotmail.com>
2024-05-23 22:56:27 +0900

    deploy

--- js/script.js
+++ js/script.js
@@ -43,9 +43,9 @@ document.getElementById('save').addEventListener('click', function () {
         // divのidとクラス名を定義
         newdiv.id = newdiv_id;
         newdiv.className = 'title-t';
-        console.log(newdiv_nom);
+        console.log(newdiv.id);
         newdiv.innerHTML =
-        `<input type="checkbox">
+            `<input id="memo${newdiv_nom}" type="checkbox">
         <p id="title${newdiv_nom}">${m_title}</p>
         <p id="d_line${newdiv_nom}">${m_d_line}</p>
         <p id="progress${newdiv_nom}">${m_progress}</p>
@@ -92,45 +92,51 @@ document.getElementById("reload").addEventListener("click", function () {
     document.getElementById("d-line").value = "";
     document.getElementById("progress").value = "";
     document.getElementById("detail-memo").value = "";
-    const json = localStorage.getItem("key_name");
+    const json = localStorage.getItem("memo1");
     console.log(json);
     // JSON形式のテキスト(メモ内容)をjavascript形式に変換
-    const key_name = JSON.parse(json);
-    console.log(key_name);
+    const key_data = JSON.parse(json);
+    console.log(key_data);
     // メモ内容を各inputタグに表示
-    document.getElementById("title").value = key_name.title;
-    document.getElementById("date").value = key_name.date;
-    document.getElementById("requester").value = key_name.requester;
-    document.getElementById("d-line").value = key_name.d_line;
-    document.getElementById("progress").value = key_name.progress;
-    document.getElementById("detail-memo").value = key_name.detail_memo;
+    document.getElementById("title").value = key_data.title;
+    document.getElementById("date").value = key_data.date;
+    document.getElementById("requester").value = key_data.requester;
+    document.getElementById("d-line").value = key_data.d_line;
+    document.getElementById("progress").value = key_data.progress;
+    document.getElementById("detail-memo").value = key_data.detail_memo;
 });
 
 // 上部メモ欄に表示し続ける
-if (localStorage.getItem("memo1")){
-    const json = localStorage.getItem("memo1");
-    const key_name = JSON.parse(json);
-    const m_title = "件名:" + key_name.title;
-    const m_d_line = "納期:" + key_name.d_line;
-    const m_progress = "進捗:" + key_name.progress;
+window.onload = function () {
+    if (localStorage.length > 0) {
+        for (let memo_nom = 0; memo_nom < localStorage.length; memo_nom++) {
+            let key_name = 'memo' + String(memo_nom);
+            const json = localStorage.getItem(key_name);
+            const key_data = JSON.parse(json);
+            const m_title = "件名:" + key_data.title;
+            const m_d_line = "納期:" + key_data.d_line;
+            const m_progress = "進捗:" + key_data.progress;
 
-    //新しいdiv,pタグを挿入  
-    let newdiv_nom = localStorage.length;
-    console.log(newdiv_nom);
-    let newdiv_id = 'newdiv' + String(newdiv_nom);
-    const newdiv = document.createElement("div");
+            //新しいdiv,pタグを挿入  
+            let newdiv_nom = localStorage.length;
+            console.log(newdiv_nom);
+            // let newdiv_id = 'memo' + String(newdiv_nom);
+            const newdiv = document.createElement("div");
 
-    // divのidとクラス名を定義
-    newdiv.id = newdiv_id;
-    newdiv.className = 'title-t';
-    newdiv.innerHTML =
-        `<input type="checkbox"></input>
+            // divのidとクラス名を定義、divの生成
+            newdiv.id = 'newdiv' + String(newdiv_nom);
+            console.log(newdiv.id);
+            newdiv.className = 'title-t';
+            newdiv.innerHTML =
+                `<input id="check_box${newdiv_nom}" type="checkbox"></input>
         <p id="title${newdiv_nom}">${m_title}</p>
         <p id="d_line${newdiv_nom}">${m_d_line}</p>
         <p id="progress${newdiv_nom}">${m_progress}</p>
         `;
-    const insert_div = document.getElementById("title_area");
-    insert_div.appendChild(newdiv);
-
+        // ローカルストレージに保存したメモdiv,pタグを挿入
+            const insert_div = document.getElementById("title_area");
+            insert_div.appendChild(newdiv);
+    
+        };
+    };
 };
-
